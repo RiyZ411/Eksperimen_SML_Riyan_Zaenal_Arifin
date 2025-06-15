45c99c8733e3a36eb7893dfcc4d8ceef54e24720
RiyZ411 <riyanzaenal411@gmail.com>
2025-06-15 18:00:55 +0700

    first commit

--- preprocessing/automate_Riyan_Zaenal_Arifin.py
+++ preprocessing/automate_Riyan_Zaenal_Arifin.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 import joblib
 
 try:
-    df = pd.read_csv('./heart_raw.csv')
+    df = pd.read_csv('../heart_raw.csv')
 except FileNotFoundError:
     raise FileNotFoundError("File heart_raw.csv tidak ditemukan.")
 
@@ -20,7 +21,7 @@ def encode_categorical(df):
 def scale_features(df, col):
     try:
         if col not in df.columns:
-            raise KeyError(f"Kolom target '{col}' tidak ditemukan.")
+            raise KeyError(f"Kolom '{col}' tidak ditemukan di DataFrame.")
         X = df.drop(columns=[col])
         y = df[col]
         scaler = StandardScaler()
@@ -30,18 +31,59 @@ def scale_features(df, col):
         return scaler, scaled
     except Exception as e:
         raise ValueError(f"Error saat scaling: {str(e)}")
+    
+def split_data(df, col, test_size):
+    try:
+        if col not in df.columns:
+            raise ValueError(f"Kolom '{col}' tidak ditemukan di DataFrame.")
+
+        X = df.drop(columns=[col])
+        y = df[col]
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=test_size, random_state=42, stratify=y
+        )
+
+        return X_train, X_test, y_train, y_test
+    except Exception as e:
+        raise ValueError(f"Error saat spliting data: {str(e)}")
 
 encoded = encode_categorical(df)
 scaler, scaled = scale_features(encoded, 'HeartDisease')
+X_train, X_test, y_train, y_test = split_data(scaled, 'HeartDisease', 0.3)
 
 try:
     joblib.dump(scaler, 'scaler.joblib')
     print('Berhasil menyimpan hasil skala normalisasi')
 except NameError:
-    raise NameError("Objek 'scaler' tidak didefinisikan.")
+    raise NameError("Objek scaler tidak didefinisikan.")
 
 try:
-    scaled.to_csv('heart_preprocessing.csv')
+    scaled.to_csv('heart_preprocessing/heart_preprocessing.csv')
     print('Berhasil menyimpan hasil normalisasi')
 except NameError:
-    raise NameError("Objek 'scaled' tidak didefinisikan.")
+    raise NameError("Objek scaled tidak didefinisikan.")
+
+try:
+    scaled.to_csv('heart_preprocessing/X_train.csv')
+    print('Berhasil menyimpan hasil spliting X_train')
+except NameError:
+    raise NameError("Objek X_train tidak didefinisikan.")
+
+try:
+    scaled.to_csv('heart_preprocessing/X_test.csv')
+    print('Berhasil menyimpan hasil spliting X_test')
+except NameError:
+    raise NameError("Objek X_test tidak didefinisikan.")
+
+try:
+    scaled.to_csv('heart_preprocessing/y_train.csv')
+    print('Berhasil menyimpan hasil spliting y_train')
+except NameError:
+    raise NameError("Objek y_train tidak didefinisikan.")
+
+try:
+    scaled.to_csv('heart_preprocessing/y_test.csv')
+    print('Berhasil menyimpan hasil spliting y_test')
+except NameError:
+    raise NameError("Objek y_test tidak didefinisikan.")
